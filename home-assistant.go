f3d38615f9353b758163cbb0ad55b6fc1c81f96e
Robert Meijers <robert.meijers@gmail.com>
2019-09-24 16:03:10 +0200

    ha: support registering binary sensors

--- home-assistant.go
+++ home-assistant.go
@@ -43,6 +43,17 @@ func (bridge *HomeAssistantBridge) RegisterSwitch(device *Device, property strin
 	}
 }
 
+func (bridge *HomeAssistantBridge) RegisterBinarySensor(device *Device, property string) {
+	topic := strings.Builder{}
+	fmt.Fprintf(&topic, "%s/binary_sensor/%s/%s/config", bridge.discoveryPrefix, device.Id, property)
+
+	config := bridge.createConfig(device, property)
+
+	if encoded, err := json.Marshal(config); err == nil {
+		bridge.mqtt.Publish(topic.String(), 0, true, encoded)
+	}
+}
+
 func (bridge *HomeAssistantBridge) createConfig(device *Device, property string) map[string]interface{} {
 	config := map[string]interface{}{
 		"state_topic": bridge.mqtt.BuildTopic(device, property),
